5370371feb21e3cd25fb736ed7c6799bc28cc27e
kjj <kwm369@caremedi.co.kr>
2025-06-11 11:51:41 +0900

    [build] library 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.bundles.androids)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
